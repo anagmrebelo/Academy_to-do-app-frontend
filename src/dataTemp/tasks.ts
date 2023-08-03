aa5ae56416b5961d82f552616252c248f59b2f2c
Ana Rebelo <anagmrebelo@hotmail.com>
2023-08-03 13:19:46 +0200

    update date format on tasks temp file

--- src/dataTemp/tasks.ts
+++ src/dataTemp/tasks.ts
@@ -2,13 +2,13 @@ export const tasks = [
   {
     id: 1,
     value: "Clean the house",
-    dueDate: "05-05-2023",
+    dueDate: "2023-08-14",
     status: false,
   },
   {
     id: 2,
     value: "Call Mom",
-    dueDate: "07-05-2023",
+    dueDate: "2023-08-25",
     status: true,
   },
 ];
